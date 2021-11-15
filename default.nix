a634aa8d11f8c3398ad771ea5c0d861e609ec957
John Shaffer <jshaffer2112@protonmail.com>
2021-11-15 15:45:29 -0600

    Add default.nix for development and build dependencies

---
+++ default.nix
@@ -0,0 +1,13 @@
+let
+  rev = "5f244caea76105b63d826911b2a1563d33ff1cdc";
+  nixpkgs = fetchTarball {
+    url = "https://github.com/NixOS/nixpkgs/archive/${rev}.tar.gz";
+    sha256 = "1xlgynfw9svy7nvh9nkxsxdzncv9hg99gbvbwv3gmrhmzc3sar75";
+  };
+  pkgs = import nixpkgs {};
+  inherit (pkgs) fetchurl;
+in with pkgs;
+let
+in with pkgs; [
+  nodePackages.npm
+]
